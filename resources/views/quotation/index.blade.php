4051daca2e54e69d88c1629c5667f31c2a127de1
kraithus <xordkraithus@gmail.com>
2022-09-06 17:53:50 +0200

    Added custom css and js files, yet to get them linked

--- resources/views/quotation/index.blade.php
+++ resources/views/quotation/index.blade.php
@@ -1,3 +1,37 @@
+<body>
+	<!---MAIN NAV--->
+	<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">
+			<h2>iQuoteYou</h2>
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebarMenu"
+			aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="fa fa-bars"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link active" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+				<input class="form-control all_forms mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-search my-2 my-sm-0" type="submit"><span class="la la-search"></span></button>
+			</form>
+		</div>
+	</nav>
+	<!---MAIN NAV--->
+
 @if(session()->has('message'))
                 <div class="sufee-alert alert with-close alert-success alert-dismissible fade show">
                                 {{ session()->get('message') }}
