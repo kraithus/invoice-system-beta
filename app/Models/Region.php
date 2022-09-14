cd323d4c0041552736dd7f4d4feece0ed3152b2f
kraithus <xordkraithus@gmail.com>
2022-09-14 13:22:44 +0200

    Added District and Region Seeders

---
+++ app/Models/Region.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Region extends Model
+{
+    use HasFactory;
+
+    /**
+     * Region has many districts
+     */
+    public function districts()
+    {
+        return $this->hasMany(District::class);
+    }
+}
